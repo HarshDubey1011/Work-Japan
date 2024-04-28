d0382ea3f8a9835d3ec23bc954fc07be3eacdcea
HarshDubey1011 <pandethallu@gmail.com>
2024-04-28 10:30:12 +0530

    new changes

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
   const searchInput = document.getElementById("search");
   const jobList = document.getElementById("job-list");
+  const searchButton = document.getElementById("search-btn");
+
+var url = "https://jooble.org/api/";
+var key = "301bff4b-2e98-4108-bbec-56a56a3d8cae";
+var params = "{ keywords: 'it', location: 'Japan'}"
+
 
   // Fetch job listings from API
   async function fetchJobListings() {
     try {
-      const response = await fetch("https://api.example.com/job-listings");
-      const data = await response.json();
-      displayJobListings(data);
+      var http = new XMLHttpRequest();
+      //open connection. true - asynchronous, false - synchronous
+      http.open("POST", url + key, true);
+
+      //Send the proper header information
+      http.setRequestHeader("Content-type", "application/json");
+  
+      //Callback when the state changes
+      http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+          console.log(http.responseText)
+          alert(http.responseText);
+          // displayJobListings(data);
+        }
+      }
+      //Send request to the server
+      http.send(params);
+      
     } catch (error) {
       console.error("Error fetching job listings:", error);
     }
@@ -19,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
     listings.forEach(function (listing) {
       const li = document.createElement("li");
       li.innerHTML = `
-                <h3>${listing.title}</h3>
+                <h3>${listing.title}nhgko</h3>
                 <p><strong>Location:</strong> ${listing.location}</p>
                 <p><strong>Type:</strong> ${listing.type}</p>
             `;
@@ -27,19 +48,14 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Filter job listings based on search input
-  function filterJobListings(searchTerm) {
-    const filteredListings = jobListings.filter(function (listing) {
-      return listing.title.toLowerCase().includes(searchTerm.toLowerCase());
-    });
-    displayJobListings(filteredListings);
-  }
-
-  // Initial display of job listings
-  fetchJobListings();
+  searchButton.addEventListener("click", function () {
+    displayJobListings(); // Call fetchJobListings on button click
+  });
 
   // Event listener for search input
   searchInput.addEventListener("input", function () {
-    filterJobListings(this.value);
+    fetchJobListings(this.value);
   });
+
+
 });
